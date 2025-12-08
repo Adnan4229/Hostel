79fe353b0aa2f51a0d9f08398d4b9758e985f723
Adnan4229 <adnanwazir066@gmail.com>
2025-12-08 08:16:26 +0000

    [some extra content added

--- hostel_managment/models/hostel_room.py
+++ hostel_managment/models/hostel_room.py
@@ -7,3 +7,20 @@ class HostelRoom(models.Model):
     hostel_id = fields.Many2one("hostel.hostel", "hostel", help="Name of hostel")
     currency_id = fields.Many2one('res.currency', string='Currency')
     rent_amount = fields.Monetary('Rent Amount', help="Enter rent amount per month")
+    room_no= fields.Integer(string="Room No")
+
+
+
+    # <database level pr check kranaga ka ya jo value arahi dublicate tu nhi and then error dega agr dublicate hu>
+    _sql_constraints=[
+        ("room_no_unique", "unique(room_no)",
+         "Room number must be unique")]
+
+
+
+
+    # <python level pr check karanga and then respond us instead of database level>
+    @api.constrains("rent_amount")
+    def _check_rent_amount(self):
+        if self.rent_amount < 0:
+          raise ValidationError(_("Rent Amount Per Month should not be a negative value!"))
